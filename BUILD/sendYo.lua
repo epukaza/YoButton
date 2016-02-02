3ac376e79b227cac8c715fd578c0aa95dc690bf9
etisab <ariasabeti@gmail.com>
2016-02-02 14:29:52 -0800

    globals to locals

--- BUILD/sendYo.lua
+++ BUILD/sendYo.lua
@@ -3,16 +3,16 @@ file.open("yorecipient.txt", "r")
 print("sending yo to "..file.readline())
 file.close()
 wifi.sleeptype(wifi.NONE_SLEEP)
+wifi.setmode(wifi.STATION)
 
 function generateContentString()
 	file.open("yorecipient.txt", "r")
 	return "api_token=0c6ac771-71fa-420f-810c-2853989a8ca6&username="..string.upper(file.readline())
 end
 
-wifi.setmode(wifi.STATION)
+local contentString = generateContentString()
+local contentLength = string.len(contentString)
 
-contentString = generateContentString()
-contentLength = string.len(contentString)
 http.post(
 	'https://api.justyo.co/yo/',
 	'Content-Type: application/x-www-form-urlencoded\r\n'..
